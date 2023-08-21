94eb52f2a013f70d16d587b4034c62799bb69171
kelvin <kval0003@student.monash.edu>
2023-08-21 15:31:00 +1000

    updated jacobian

--- Week03-05/slam/robot.py
+++ Week03-05/slam/robot.py
@@ -132,15 +132,16 @@ class Robot:
         
         # todo: add your codes here to compute Jac2 using lin_vel, ang_vel, dt, th, and th2
         if ang_vel == 0:
-            Jac2[0,0] = np.cos(th)
-            Jac2[1,0] = np.sin(th)
+            Jac2[0,0] = np.cos(th)*dt
+            Jac2[1,0] = np.sin(th)*dt
         else:
             Jac2[0,0] = 1/ang_vel * (np.sin(th2) - np.sin(th))
-            Jac2[0,1] = -1/(ang_vel**2) * (np.sin(th2) - np.sin(th)) + lin_vel / ang_vel * (dt * np.cos(th2))
-
-            Jac2[1,0] = -1/ang_vel * (np.cos(th2) - np.cos(th))
-            Jac2[1,1] = 1/(ang_vel**2) * (np.cos(th2) - np.cos(th)) + -lin_vel / ang_vel * (-dt * np.sin(th2))
-
+            #Jac2[0,1] = -1/(ang_vel**2) * (np.sin(th2) - np.sin(th)) + lin_vel / ang_vel * (dt * np.cos(th2))
+            Jac[0,1] = lin_vel/(ang_vel**2)*(np.sin(th)-np.sin(th2))
+            #Jac2[1,0] = -1/ang_vel * (np.cos(th2) - np.cos(th))
+            Jac2[1,0] = np.cos(th)/ang_vel-np.cos(th2)/ang_vel
+            #Jac2[1,1] = 1/(ang_vel**2) * (np.cos(th2) - np.cos(th)) + -lin_vel / ang_vel * (-dt * np.sin(th2))
+            Jac2[1,1] = lin_vel/(ang_vel**2)*(-np.cos(th)+np.cos(th2))
         Jac2[2,1] = dt
         # Derivative of x,y,theta w.r.t. left_speed, right_speed
         Jac = Jac2 @ Jac1
